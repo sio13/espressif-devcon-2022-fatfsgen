ef07ac43f707460e5f291af2282eb8bb9b314840
Martin Gaňo <martin.gano@espressif.com>
2022-09-01 15:30:02 +0200

    Fixed extract.py

--- extract.py
+++ extract.py
@@ -1,10 +1,6 @@
-import sys
-import os
-idf_path = os.environ["IDF_PATH"]  # get value of IDF_PATH from environment
-parttool_dir = os.path.join(idf_path, "components", "partition_table")  # parttool.py lives in $IDF_PATH/components/partition_table
-sys.path.append(parttool_dir)  # this enables Python to find parttool module
-from parttool import *  # import all names inside parttool module
+import sys, os  
+sys.path.append(os.path.join(os.environ["IDF_PATH"], "components", "partition_table"))  
+from parttool import ParttoolTarget, PartitionName
 
-# Create a partool.py target device connected on serial port /dev/ttyUSB1
 target = ParttoolTarget("/dev/cu.usbserial-1410")
 target.read_partition(PartitionName("storage"), "fatfs.img")
